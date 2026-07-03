f2fd4c52c4682cc2ab5b4c9be78bd5408338bec8
vhtn2010 <murasakiamenn@gmail.com>
2026-07-03 09:52:59 +0700

    Them xoa asset Cloudinary

--- backend/src/controllers/uploadController.js
+++ backend/src/controllers/uploadController.js
@@ -1,4 +1,5 @@
 const uploadSignatureService = require('../services/uploadSignatureService');
+const uploadDeleteService = require('../services/uploadDeleteService');
 
 const createUploadSignature = async (req, res) => {
   const data = await uploadSignatureService.createSignature({
@@ -14,6 +15,21 @@ const createUploadSignature = async (req, res) => {
   });
 };
 
+const deleteCloudinaryUpload = async (req, res) => {
+  const data = await uploadDeleteService.deleteCloudinaryAsset({
+    auth: req.auth,
+    body: req.body,
+    ipAddress: req.ip,
+    userAgent: req.get('user-agent'),
+  });
+
+  res.success({
+    data,
+    message: 'Cloudinary asset deleted successfully',
+  });
+};
+
 module.exports = {
   createUploadSignature,
+  deleteCloudinaryUpload,
 };
