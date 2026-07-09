2b8a7c95644482f3a365acf3abab7149043d037d
vhtn2010 <murasakiamenn@gmail.com>
2026-07-09 13:22:24 +0700

    P3-public-ui-kit-hotel-shell

---
+++ frontend/src/components/public/ui/index.js
@@ -0,0 +1,11 @@
+export {
+  PublicButton,
+  PublicCard,
+  PublicEmptyState,
+  PublicErrorState,
+  PublicLoadingBlock,
+  PublicNotice,
+  PublicPagination,
+  PublicSectionHeader,
+  PublicState,
+} from './PublicUiKit.jsx'
