65077e1ad5c8360ffbc061cdf9d225cf67427804
vhtn2010 <murasakiamenn@gmail.com>
2026-07-09 12:09:06 +0700

    Hybridize admin-safe with backup user public frontend

--- frontend/src/repositories/publicServiceRepository.js
+++ frontend/src/repositories/publicServiceRepository.js
@@ -1,17 +1,23 @@
 import {
-  getFeaturedTourServices as getFeaturedTourServicesWithMockAdapter,
-  getTourServiceBySlug as getTourServiceBySlugWithMockAdapter,
-  listTourServices as listTourServicesWithMockAdapter,
-} from '../adapters/mock/publicServiceMockAdapter.js'
+  getFeaturedTourServices as getFeaturedTourServicesWithApiAdapter,
+  getTourServiceCatalog as getTourServiceCatalogWithApiAdapter,
+  getTourServiceBySlug as getTourServiceBySlugWithApiAdapter,
+  listTourServices as listTourServicesWithApiAdapter,
+} from '../adapters/api/publicServiceApiAdapter.js'
 
 const publicServiceAdapter = {
-  getFeaturedTourServices: getFeaturedTourServicesWithMockAdapter,
-  getTourServiceBySlug: getTourServiceBySlugWithMockAdapter,
-  listTourServices: listTourServicesWithMockAdapter,
+  getFeaturedTourServices: getFeaturedTourServicesWithApiAdapter,
+  getTourServiceCatalog: getTourServiceCatalogWithApiAdapter,
+  getTourServiceBySlug: getTourServiceBySlugWithApiAdapter,
+  listTourServices: listTourServicesWithApiAdapter,
 }
 
 export function listTourServices(params) {
   return publicServiceAdapter.listTourServices(params)
+}
+
+export function getTourServiceCatalog() {
+  return publicServiceAdapter.getTourServiceCatalog()
 }
 
 export function getTourServiceBySlug(slug) {
